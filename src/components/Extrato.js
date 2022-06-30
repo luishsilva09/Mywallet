21f9ffff6c036314f03872e13ff276a5f2caf788
luis <luishsilva09@gmail.com>
2022-06-30 20:08:22 -0300

    buscar por extrato e exibir

--- src/components/Extrato.js
+++ src/components/Extrato.js
@@ -1,13 +1,41 @@
+import axios from "axios";
 import React from "react";
 import styled from "styled-components";
+import UserContext from "../context/UserContext";
 
 export default function Extrato() {
+  const [dados, setDados] = React.useState();
+  const [load, setLoad] = React.useState(true);
+  const { userData } = React.useContext(UserContext);
+  const config = {
+    headers: {
+      Authorization: `Bearer ${userData.token}`,
+    },
+  };
+  React.useEffect(() => {
+    const promise = axios.get("http://localhost:5000/extrato", config);
+    promise.then((res) => {
+      setDados(res.data);
+      setLoad(false);
+    });
+  }, []);
+
   return (
     <Container>
-      <p>
-        Não há registros de <br />
-        entrada ou saída
-      </p>
+      {load ? (
+        <p>
+          Não há registros de <br />
+          entrada ou saída
+        </p>
+      ) : (
+        dados.map((e) => (
+          <Item>
+            <h2>{e.data}</h2>
+            <h3>{e.descricao}</h3>
+            <h4 color={e.type}>{e.valor}</h4>
+          </Item>
+        ))
+      )}
     </Container>
   );
 }
@@ -26,5 +54,21 @@ const Container = styled.div`
     justify-content: center;
     font-size: 20px;
     color: #868686;
+  }
+`;
+
+const Item = styled.div`
+  display: flex;
+  font-size: 16px;
+  width: 100%;
+
+  h2 {
+    color: #c6c6c6;
+  }
+  h3 {
+    color: #000;
+  }
+  h4 {
+    color: ${(props) => (props.color === "entrada" ? "red" : "green")};
   }
 `;
